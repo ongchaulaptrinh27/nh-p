7f01e9fa89d4af95c80de7da11ad8b450137409d
ongchaulaptrinh27 <duongthithuyhangkupee@gmail.com>
2025-10-23 10:55:35 +0700

    Create sanpham.js

---
+++ sanpham.js
@@ -0,0 +1,353 @@
+const products = [
+            {
+                id: 1,
+                title: "01ngân hà tình yêu",
+                description: "Mẫu này thích hợp cho các bạn đã có ngiu hoặc gia đình",
+                detailedDescription: "",
+                price: "100.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7523192990362340626?is_from_webapp=1&sender_device=pc&web_id=7529880604436563463",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250821b4c2c102caa9cc4f4d9f5d03e21c5a8d.jpg",
+                category: ["premium", "love","all","Website"],
+                features: [
+                    "tối đa 10 ảnh",
+                    "họ trợ từ a=>z",
+                    "demo ở tiktok @sangnguyendev",
+                    ""
+                ],
+                rating: 4.8,
+                downloads: 12,
+                pinned: false  // Ghim sản phẩm này (bạn có thể chỉnh sửa true/false cho từng sản phẩm)
+            },
+            {
+                id: 2,
+                title: "02 mô hình vũ trụ 3js",
+                description: "chi tiết cách vận hành vũ trụ chòm sao và các vệ tinh,vv",
+                detailedDescription: "",
+                price: "200.000",
+                demoUrl: "https://he-thong-vu-tru.vercel.app/",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250827902bb7496e24687d70e7aae7ff064734.jpg",
+                category: ["premium","Three js","all"],
+                features: [
+                    "cách vệ tinh vận hành",
+                    "cách hành tinh của hệ mặt trời",
+                    "3d",
+                    "vv"
+                ],
+                rating: 4.5,
+                downloads: 1,
+                pinned: false  // Ghim sản phẩm này
+            },
+             {
+                id: 3,
+                title: "03 tỏ tình ngiu",
+                description: "Mẫu này thích hợp cho các bạn đã có ngiu hoặc gia đình",
+                detailedDescription: "",
+                price: "120.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7539401947292519688?lang=en",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250821fd3d3bd86190335e3c9dfe7e0c9ec970.jpg",
+                category: ["premium", "love","all","Website"],
+                features: [
+                    "tỏ tình ngiu nhìu chức năng ",
+                    "xem link demo rồi báo admin nha",
+                    "hoặc có thể xem demo ở tiktok @sangnguyendev",
+                    ""
+                ],
+                rating: 4.2,
+                downloads: 7,
+                pinned: false
+            },
+            {
+                id: 4,
+                title: "04đếm này yêu",
+                description: "đếm này yêu đa chức năng có thể lưu trử kỷ niệm",
+                detailedDescription: "",
+                price: "200.000",
+                demoUrl: "#",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://nguyennamtien.shop/upload/product/productH0YE9A.png",
+                category: ["premium", "love","wedsite","all"],
+                features: [
+                    "đếm này yêu",
+                    "lưu dữ kỷ niệm",
+                    "lãng mạng",
+                    ""
+                ],
+                rating: 4.8,
+                downloads: 1250,
+                pinned: false
+            },
+            {
+                id: 5,
+                title: "karaoke lyric",
+                description: "cùng hát thoi nào",
+                detailedDescription: "",
+                price: "70.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7537904599278144776?lang=en",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250821e1667e52f25e5f5e0d62e3797c3bbb58.jpg",
+                category: ["premium",],
+                features: [
+                    "",
+                    "",
+                ],
+                rating: 4.8,
+                downloads: 2,
+                pinned: false
+            },
+             {
+                id: 6,
+                title: "06 sinh nhật",
+                description: "code sinh nhật siêu xịn",
+                detailedDescription: "Mẫu portfolio đẹp mắt với thiết kế hiện đại, phù hợp cho các nhà phát triển, nhà thiết kế và những người sáng tạo muốn trưng bày công việc của họ.",
+                price: "100.000",
+                demoUrl: "#",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250821776e271ff10beeb529d4906aa6d82938.jpg",
+                category: ["premium", "love","website"],
+                features: [
+                    "demo ở tiktok mình nha",
+                    "@sangnguyendev",
+            
+                ],
+                rating: 4.5,
+                downloads: 5,
+                pinned: false
+            },
+             {
+                id: 7,
+                title: "07 sinh nhật 2",
+                description: "đón sinh nhật cùng ngiu nào",
+                detailedDescription: "",
+                price: "Miễn phí(đang bảo trì)",
+                demoUrl: "#",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_202508216c9e169ce16a6de478f819d48e2edd5e.jpg",
+                category: ["free", "love","all","website"],
+                features: [
+                    "sinh nhật vui vẽ",
+                   
+                 
+                    
+                ],
+                rating: 4.0,
+                downloads: 3,
+                pinned: false
+            },
+         {
+                id: 8,
+                title: "hoa",
+                description: "ngào ngào",
+                detailedDescription: "",
+                price: "50.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7492021809383263495?is_from_webapp=1&sender_device=pc&web_id=7529880604436563463",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250823b51595616f60972c636a3d8304c29e51.jpg",
+                category: ["premium", "love","all"],
+                features: [
+                    "lãng mạng",
+                   
+                 
+                    
+                ],
+                rating: 4.0,
+                downloads: 2,
+                pinned: false
+            },
+         {
+                id: 9,
+                title: "bản đổ biển đảo",
+                description: "tự hào bản biển đâỏ Việt Nam",
+                detailedDescription: "",
+                price: "70.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7491640644104916232?is_from_webapp=1&sender_device=pc&web_id=7529880604436563463",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_202508236417b900aa9c3399400fe05797dca0c5.jpg",
+                category: ["premium", "all"],
+                features: [
+                    "lãng mạng",
+                   
+                 
+                    
+                ],
+                rating: 4.1,
+                downloads: 2,
+                pinned: false
+            },
+          {
+                id: 10,
+                title: "trái tim",
+                description: "",
+                detailedDescription: "",
+                price: "100.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7487842920985316609?is_from_webapp=1&sender_device=pc&web_id=7529880604436563463",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20250827ba536f4710fabb7ae09d18a43bf3af5f.jpg",
+                category: ["premium", "all","website"],
+                features: [
+                    "lãng mạng",
+                   
+                 
+                    
+                ],
+                rating: 4.1,
+                downloads: 0,
+                pinned: false  // Ghim sản phẩm này
+            },
+         {
+                id: 11,
+                title: "shop",
+                description: "shop bán nick game",
+                detailedDescription: "",
+                price: "100.000",
+                demoUrl: "#",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_202508285a4e51a728cf2d26feafdd43e5889405.jpg",
+                category: ["premium", "all","website"],
+                features: [
+                    "tính năng đầy đủ ",
+                    "có thể nạp qua thẻ cào ",
+                   
+                 
+                    
+                ],
+                rating: 4.1,
+                downloads: 1,
+                pinned: true
+            },
+         {
+                id: 12,
+                title: "thiết kế điện thoại",
+                description: "thiết ké iphone tương lai",
+                detailedDescription: "",
+                price: "50.000",
+                demoUrl: "#",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_202509039c08f41f4a641499ada2a2550b7a16c4.jpg",
+                category: ["premium", "all","website","Three js"],
+                features: [
+                    "NHIỀU TÍNH NĂNG ",
+                    "BẠN CÓ THỂ THIẾT KẾ ĐIỆN THOẠI ",
+                   
+                 
+                    
+                ],
+                rating: 4.1,
+                downloads: 1,
+                pinned: false
+            },
+          {
+                id: 13,
+                title: "lưu dữ kỉ niệm tình yêu",
+                description: "lưu dữ nhưng kỉ niệm chúng ta",
+                detailedDescription: "",
+                price: "100.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7547720764372389140?is_from_webapp=1&sender_device=pc&web_id=7529880604436563463",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_202509137c2eac942e3aa7f419ee54163dfe1daf.jpg",
+                category: ["premium", "all","website","love"],
+                features: [
+                    "giá gốc 500k bán lại 100k",
+                    "xem trên tiktok mik nha ",
+                   
+                 
+                    
+                ],
+                rating: 4.1,
+                downloads: 1,
+                pinned: true
+            },
+         {
+                id: 14,
+                title: "thử ngiệm power",
+                description: "slide ",
+                detailedDescription: "",
+                price: "100.000.000.000",
+                demoUrl: "#",
+                fileUrl: "#",
+                imageUrl: "https://wallpapers.com/images/hd/coding-background-9izlympnd0ovmpli.jpg",
+                category: ["premium",],
+                features: [
+                    "",
+                    "",
+                ],
+                rating: 4.8,
+                downloads: 2,
+                pinned: false
+            },
+         {
+                id: 15,
+                title: "trái tim ",
+                description: "trái tim three js",
+                detailedDescription: "",
+                price: "70.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7558473007237319954",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20251016649b47b963e34d529f99c2dcfe72e775.jpg",
+                category: ["premium", "all","website","love","3js"],
+                features: [
+                    "tình yêu",
+                    "xem trên tiktok mik nha ",
+                   
+                 
+                    
+                ],
+                rating: 5,
+                downloads: 9,
+                pinned: true
+            },
+         {
+                id: 16,
+                title: "karaoke lyric",
+                description: "",
+                detailedDescription: "",
+                price: "70.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7561683548483259669",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_2025101604eca2b72be77940304527865dd02cd1.jpg",
+                category: ["premium", "all","love"],
+                features: [
+                    "bài em đã xa anh mất rồi người ơi",
+                    "xem trên tiktok mik nha ",
+                   
+                 
+                    
+                ],
+                rating: 4,
+                downloads: 2,
+                pinned: false
+            },
+         {
+                id: 17,
+                title: "20/10",
+                description: "ngày liên hiệp phụ nữ Việt Nam",
+                detailedDescription: "",
+                price: "69.000",
+                demoUrl: "https://www.tiktok.com/@sangnguyendev/video/7562769438504668417",
+                fileUrl: "https://www.facebook.com/profile.php?id=61574167490098",
+                imageUrl: "https://sf-static.upanhlaylink.com/img/image_20251019650baaf69ae3d41230a9ba61cc58e330.jpg",
+                category: ["premium", "all","love"],
+                features: [
+                    "20/10 vui vẻ nha",
+                    "xem trên tiktok mik nha ",
+                   
+                 
+                    
+                ],
+                rating: 4,
+                downloads: 2,
+                pinned: false
+            },
+         
+           ]
+           const saved = localStorage.getItem('sangdev_products');
+if (saved) {
+  try {
+    window.products = JSON.parse(saved);
+  } catch (e) {
+    console.warn('Lỗi đọc danh sách sản phẩm:', e);
+  }
+}
+
